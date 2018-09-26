8d95b1ceb0af4b0a3d746d16e0570f9eb5fc9b02
Jessica Olivieri <jessicaolivieri@jessicas-macbook-pro.local>
2018-09-26 14:50:14 -0400

    1.1: Basic 8-ball game

--- EightBall/ViewController.swift
+++ EightBall/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var resultLabel: UILabel!
+	var index = -1
+	let answerArray = ["It is certain!",
+					   "Ask again later",
+					   "My sources say no!",
+					   "Yes!",
+					   "Better not tell you.",
+					   "Very doubtful!"]
+	
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
+	@IBAction func answerButtonPressed(_ sender: Any) {
+		//resultLabel.text = answerArray.randomElement()
+		
+		
+//		var randomPhrase = ""
+//		repeat {
+//			randomPhrase = answerArray.randomElement()!
+//		} while randomPhrase == resultLabel.text!
+//		resultLabel.text = randomPhrase
+		
+		// ANOTHER WAY TO MAKE SURE YOU ARE NOT REPEATING //
+		var newIndex = -1
+		repeat {
+			newIndex = Int.random(in: 0...answerArray.count - 1)
+		} while index == newIndex
+		
+		resultLabel.text = answerArray[newIndex]
+		index = newIndex // global variable (lives forever)
+		
+		// could also do WHILE loop //
+		
+	}
+	
 
 }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
